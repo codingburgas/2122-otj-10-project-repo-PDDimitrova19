cf670cc1b69cc5e9e959fbdb4f8f1f3f0c0e1e6a
Polya <PDDimitrova19@codingburgas.bg>
2022-07-04 16:40:19 +0300

    Add third repo

--- project/Admin.cpp
+++ project/Admin.cpp
@@ -10,23 +10,19 @@ void addRepo(REPOSITORY repo)
 	repos = temp;
 }
 
-// Function for getting first Kingdom events
 REPOSITORY_LIST* getProjectRepos() {
 	REPOSITORY_LIST* temp1 = repos;
 	REPOSITORY_LIST* repositories = new REPOSITORY_LIST;
 	while (temp1->next != NULL) {
-		if (temp1->repo.index <= 1000) {
-			REPOSITORY_LIST* temp2 = new REPOSITORY_LIST();
-			temp2->repo = temp1->repo;
-			temp2->next = repositories;
-			repositories = temp2;
-		}
+		REPOSITORY_LIST* temp2 = new REPOSITORY_LIST();
+		temp2->repo = temp1->repo;
+		temp2->next = repositories;
+		repositories = temp2;
 		temp1 = temp1->next;
 	}
 	return repositories;
 }
 
-// Remove First Kingdom events
 void removeProjectRepos()
 {
 	REPOSITORY_LIST* temp = repos;
@@ -46,5 +42,5 @@ void removeProjectRepos()
 void initialiseRepos() {
 	addRepo({ 13, "PIPetkova19/Xenon", "We are team Xenon! We can take instructions from all levels and build up good working relationships. Our task is/was to create a C++ Maze Game." });
 	addRepo({ 21, "VYKostova19/AZ - 3", "The main goal of our project is to present C++ Maze Game. We have created an awesome algorithm and an easy to use and understand styled UI." });
-	addRepo({ 1, "text", "text" });
+	addRepo({ 17, "DHGrudov19/CyberFlame", "Hello we are team CyberFlame and our project is set on the topic Fire Department! We have prepared a site which includes informationand explanation how to prevent most of the disasters that can cause fire.Also we have made different kind of statistics for the employees, the cases of serviceand the equipment. You can signaland report for a fire in one of our tab in the site." });
 }
